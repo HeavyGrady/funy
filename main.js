b0af8d06da55777e73ecbd2db9bc19f6d1ead2cd
HeavyGrady <chase.deluca@innovationcharter.org>
2025-01-18 16:35:42 +0000

    set up done

---
+++ main.js
@@ -0,0 +1,3 @@
+// Write JavaScript here. 
+console.log("main.js loaded successfully!"); 
+// /* Some sample JavaScript: this just listens for any "click" on the hero element and then toggles the "active" class which we use to shift colors when they click let hero = document.querySelector(".hero"); hero.addEventListener("scroll", () => { console.log("They scrolled!"); hero.classList.toggle("active"); }); */
